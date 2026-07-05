21ad44744c79c3ab8340e54c0372ed275833113e
alexsa2517 <admoney4@gmail.com>
2026-07-05 14:06:58 +0700

    Create seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,20 @@
+-- =========================
+-- CATEGORIES
+-- =========================
+INSERT INTO categories (name) VALUES
+('Seafood'),
+('Chicken'),
+('Pork'),
+('Beef'),
+('Rice'),
+('Noodles'),
+('Soup'),
+('Vegetarian');
+
+-- =========================
+-- COUNTRIES
+-- =========================
+INSERT INTO countries (name, code) VALUES
+('Thailand', 'TH'),
+('China', 'CN'),
+('Japan', 'JP');
